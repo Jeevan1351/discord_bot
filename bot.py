8a953ffa855a84f7885c840f9cbbdd4ca375aeea
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2023-01-06 01:13:06 +0530

    Updates

--- bot.py
+++ bot.py
@@ -19,20 +19,52 @@ async def on_message(message):
         return
  
     if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
+        await message.channel.send(f'Hello! <@!{message.author.id}>')
     
     if "good morning" in message.content:
         await message.channel.send("Good morning!")
     await client.process_commands(message)
 
 
-@client.command()
+@client.command(description="Pings the bot")
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-@client.command()
+@client.command(description="Clears messages")
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
+@client.command(description="Toss a coin")
+async def toss(ctx):
+    import random
+    toss = random.randint(1, 2)
+    if toss == 1:
+        await ctx.send("Heads")
+        await ctx.send(file=discord.File('heads-coin-toss.gif'))
+    else:
+        await ctx.send("Tails")
+        await ctx.send(file=discord.File('tails-coin-toss.gif'))
+
+class JoinDistance:
+    def __init__(self, joined, created):
+        self.joined = joined
+        self.created = created
+
+    @property
+    def delta(self):
+        return self.joined - self.created
+
+class JoinDistanceConverter(commands.MemberConverter):
+    async def convert(self, ctx, argument):
+        member = await super().convert(ctx, argument)
+        return JoinDistance(member.joined_at, member.created_at)
+
+@client.command(description="Check how new a member is")
+async def delta(ctx, *, member: JoinDistanceConverter):
+    is_new = member.delta.days < 100
+    if is_new:
+        await ctx.send(f"Hey you're pretty new! {member.delta.days} days!")
+    else:
+        await ctx.send(f"Hm you're not so new. {member.delta.days} days!")
 
 client.run(os.getenv('TOKEN'))
